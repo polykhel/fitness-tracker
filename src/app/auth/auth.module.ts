f74d0652ccb396578b619fd05d24d6f48b7ef83b
Michael Pollente <mpollente@exist.com>
2021-07-30 23:54:29 +0800

    Create a SharedModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AngularFireAuthModule } from "@angular/fire/auth";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MaterialModule } from "../material.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 import { LoginComponent } from "./login/login.component";
 
 import { SignupComponent } from "./signup/signup.component";
@@ -11,14 +9,10 @@ import { SignupComponent } from "./signup/signup.component";
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
   imports: [
-    CommonModule,
-    FormsModule,
     ReactiveFormsModule,
-    MaterialModule,
     AngularFireAuthModule,
-    FlexLayoutModule
-  ],
-  exports: []
+    SharedModule
+  ]
 })
 export class AuthModule {
 }
