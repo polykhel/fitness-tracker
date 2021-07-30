2745cc11d9d840728ec60ca3f5a760543b4e2648
Michael Pollente <mpollente@exist.com>
2021-07-31 00:25:34 +0800

    Add firebase configuration to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDaxJsnVxA16C-m41RxvktTiRhsN0Juw2Y",
+    authDomain: "ng-fitness-tracker-6caef.firebaseapp.com",
+    projectId: "ng-fitness-tracker-6caef",
+    storageBucket: "ng-fitness-tracker-6caef.appspot.com",
+    messagingSenderId: "33770241619",
+    appId: "1:33770241619:web:71e1a072e4b80da62e50a6",
+    measurementId: "G-51Z9GMSQ90"
+  }
 };
