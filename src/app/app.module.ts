c873821b16d33039ab3a626be98aae4714a6aeeb
Michael Pollente <mpollente@exist.com>
2021-07-30 23:57:30 +0800

    Split TrainingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from "@angular/fire";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from "../environments/environment";
@@ -14,38 +10,25 @@ import { AuthModule } from "./auth/auth.module";
 import { MaterialModule } from "./material.module";
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { StopTrainingComponent } from "./training/current-training/stop-training.component";
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
-import { TrainingComponent } from './training/training.component';
+import { TrainingModule } from "./training/training.module";
 import { TrainingService } from "./training/training.service";
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingsComponent,
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
-    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AuthModule
+    MaterialModule,
+    AuthModule,
+    TrainingModule
   ],
   providers: [TrainingService],
   bootstrap: [AppComponent]
